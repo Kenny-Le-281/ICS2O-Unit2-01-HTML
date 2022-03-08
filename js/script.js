ed482bf4611eb551e7fb14a93c0e4bc51eec8c5d
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-08 18:47:06 +0000

    added js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Created by: Kenny Le
+ * Created on: Mar 2022
+ * This file contains the JS functions for index.html 
+*/
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+  document.getElementById("hello-world").innerHTML = <div id="hello-world"></div>
+}
